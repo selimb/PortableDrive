e989936a3528f720d6c9de4e257fc1cd11fad85b
Kreger51 <selim.belhaouane@gmail.com>
2015-05-20 21:27:35 -0400

    Re-enable local python setup

--- start.bat
+++ start.bat
@@ -1,7 +1,6 @@
 @echo off
 call bin\start\set_env.bat
-:: call "WinPython-64bit-3.4.2.4\scripts\env.bat"
+call "WinPython-64bit-3.4.2.4\scripts\env.bat"
 call bin\start\start_caffeine.bat
-:: call bin\start\file_replace.exe
 start bin\start\hotkeys.exe
 Apps\Start.exe
